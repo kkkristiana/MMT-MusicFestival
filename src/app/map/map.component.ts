3ad993a73797abbcad0a6e65d8d40b214e1254a1
Kristiana Atasi <kristiana@evori.com>
2018-06-14 21:18:03 +0300

    Location info added

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ViewChild } from '@angular/core';
-import { AgmMap, AgmMarker } from '@agm/core';
 
 @Component({
   selector: 'app-map',
@@ -13,9 +11,16 @@ export class MapComponent implements OnInit {
   lng = 27.8608;
 
   ngOnInit() {
-
   }
 
-  constructor() { }
+  findAccomodation() {
+    // tslint:disable-next-line:max-line-length
+    window.open('https://www.booking.com/searchresults.en-gb.html?label=gen173nr-1DCAEoggJCAlhYSDNYBGgXiAEBmAEuuAEHyAEM2AED6AEBkgIBeagCAw&sid=b6796ce320a00fa5443aeeb6b9aeb6ea&sb=1&src=index&src_elem=sb&error_url=https%3A%2F%2Fwww.booking.com%2Findex.en-gb.html%3Flabel%3Dgen173nr-1DCAEoggJCAlhYSDNYBGgXiAEBmAEuuAEHyAEM2AED6AEBkgIBeagCAw%3Bsid%3Db6796ce320a00fa5443aeeb6b9aeb6ea%3Bsb_price_type%3Dtotal%26%3B&ss=Varvara%2C+Burgas+Province%2C+Bulgaria&checkin_monthday=16&checkin_month=8&checkin_year=2018&checkout_monthday=19&checkout_month=8&checkout_year=2018&no_rooms=1&group_adults=2&group_children=0&from_sf=1&ss_raw=varvara&ac_position=0&ac_langcode=en&dest_id=-839474&dest_type=city&place_id_lat=42.12065&place_id_lon=27.90074&search_pageview_id=6d417e928ae4003f&search_selected=true', '_blank');
+  }
+
+  showGuide() {
+    // tslint:disable-next-line:max-line-length
+    window.open('http://telegra.ph/getting-to-Varvara-08-10?utm_source=tr.im&utm_medium=l.facebook.com&utm_campaign=tr.im%2FGET2BSF17&utm_content=link_click', '_blank');
+  }
 
 }
--- src/app/map/map.module.ts
+++ src/app/map/map.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MapComponent } from './map.component';
 import { AgmCoreModule } from '@agm/core';
+import { MatCardModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyDMGerodnedJjjgJ4mK2UFRGVIkCwCI6_4'
-    })
+    }),
+    MatCardModule,
+    MatButtonModule
   ],
   declarations: [
     MapComponent
